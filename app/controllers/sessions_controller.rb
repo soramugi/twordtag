27e4227f228a13ce02ecd5d332a08de30eedba27
soramugi <soramugi.chika@gmail.com>
2014-01-24 14:57:03 +0900

    gem内の出力を握りつぶす

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
   def index
+
     dict_dir=Twordtag::Application.config.okura_dict_dir
+    $stdout = StringIO.new
     tagger=Okura::Serializer::FormatInfo.create_tagger dict_dir
+    $stdout = STDOUT
 
     str='解析対象の文字列'
-
     @nodes=tagger.parse(str)
   end
 
