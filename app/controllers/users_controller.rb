3e0841fda7bcf73d3eff6b8213f5a75a3d1d7dd4
soramugi <soramugi.chika@gmail.com>
2014-01-31 10:14:11 +0900

    ユーザーの日付個別ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,13 @@ class UsersController < ApplicationController
 
   # GET /user/:name/:year/:month/:day
   def show_date
+    @tag_log = @user.tag_logs.find_by_date(date_param_parse)
   end
 
   private
+  def date_param_parse
+    Time.parse("#{params[:year]}/#{params[:month]}/#{params[:day]}").to_date
+  end
   def set_user
     @user = User.find_by_name(params[:name])
   end
