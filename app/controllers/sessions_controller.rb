4bffca2df4514ed046ab045d1ebed50535a84d4e
soramugi <soramugi.chika@gmail.com>
2014-01-31 10:18:52 +0900

    初回登録時にtagを作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user =
-      User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
-      User.create_with_omniauth(auth)
+    if !(user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]))
+      user = User.create_with_omniauth(auth)
+      user.create_tags
+    end
 
     login(user)
     redirect_to root_url
